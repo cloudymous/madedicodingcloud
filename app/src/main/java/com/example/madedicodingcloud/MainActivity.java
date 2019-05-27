4125f8045dd68b3fa9c036dacc043ee9b4e99b19
cloudymous <sayacloud@gmail.com>
2019-05-27 09:10:28 +0800

    Coding for main activity

--- app/src/main/java/com/example/madedicodingcloud/MainActivity.java
+++ app/src/main/java/com/example/madedicodingcloud/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.madedicodingcloud;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText edtPanjang, edtLebar, edtTinggi;
+    Button btnCalculate;
+    TextView tvResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtPanjang = findViewById(R.id.edt_panjang);
+        edtLebar = findViewById(R.id.edt_lebar);
+        edtTinggi = findViewById(R.id.edt_tinggi);
+        btnCalculate = findViewById(R.id.btn_hitung);
+        tvResult = findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_hitung) {
+
+            String inputPanjang = edtPanjang.getText().toString().trim();
+            String inputLebar = edtLebar.getText().toString().trim();
+            String inputTinggi = edtTinggi.getText().toString().trim();
+
+            boolean isEmptyFields = false;
+            boolean isInvalidDouble = false;
+
+            if (TextUtils.isEmpty(inputPanjang)) {
+                isEmptyFields = true;
+                edtPanjang.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputLebar)) {
+                isEmptyFields = true;
+                edtLebar.setError("Field ini tidak boleh kosong");
+            }
+
+            if (TextUtils.isEmpty(inputTinggi)) {
+                isEmptyFields = true;
+                edtTinggi.setError("Field ini tidak boleh kosong");
+            }
+
+            Double panjang = toDouble(inputPanjang);
+            Double lebar = toDouble(inputLebar);
+            Double tinggi = toDouble(inputTinggi);
+
+            if (panjang == null) {
+                isInvalidDouble = true;
+                edtPanjang.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (lebar == null) {
+                isInvalidDouble = true;
+                edtLebar.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (tinggi == null) {
+                isInvalidDouble = true;
+                edtTinggi.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if (!isEmptyFields && !isInvalidDouble) {
+                double volume = panjang * lebar * tinggi;
+
+                tvResult.setText(String.valueOf(volume));
+            }
+        }
+
+    }
+
+    Double toDouble(String str) {
+        try {
+            return Double.valueOf(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 }
