9709430f06bf9bff93d30a615ffb0b7b3e931c27
cloudymous <sayacloud@gmail.com>
2019-06-20 09:47:12 +0800

    Bikin class president

---
+++ app/src/main/java/com/example/madedicodingcloud/President.java
@@ -0,0 +1,30 @@
+package com.example.madedicodingcloud;
+
+public class President {
+
+    private String name, remaks, photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRemaks() {
+        return remaks;
+    }
+
+    public void setRemaks(String remaks) {
+        this.remaks = remaks;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
